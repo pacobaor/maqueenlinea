819c7d102fb6022a727cb90b4c841b6ed804d4c0
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-03-23 09:16:26 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
